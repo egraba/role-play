29ddb53b1f23ae04a1cf45fef2274ab2ef59cd11
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-12-02 23:38:45 +0100

    Register exiting models in Django administration

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game
+from .models import Master
+from .models import Character
+
+admin.site.register(Game)
+admin.site.register(Master)
+admin.site.register(Character)
