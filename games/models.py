44fdbd24783b5c52282aa4a9880b060b409d008e
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-12-02 23:29:43 +0100

    Update Master model and add Game and Character

--- games/models.py
+++ games/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
+class Game(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
 class Master(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, null=True)
+
+class Character(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=255)
+    race = models.SmallIntegerField()
+    age = models.SmallIntegerField()
+    xp = models.SmallIntegerField()
+    mp = models.SmallIntegerField()
