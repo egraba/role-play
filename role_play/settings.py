ebf24975b7755f0570cba154a18a3b6f417dd7a8
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-12-02 22:51:25 +0100

    Integrate Postgresql

--- role_play/settings.py
+++ role_play/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'role_play.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'role-play',
+        'USER': 'devuser',
+        'PASSWORD': 'devpwd',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
