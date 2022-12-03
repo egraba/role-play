49f96bb2c7d108c4cf12395a2339205159a3399a
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-12-03 20:58:50 +0100

    Rename back app name to game

--- role_play/settings.py
+++ role_play/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'games.apps.GamesConfig',
+    'game.apps.GameConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- role_play/urls.py
+++ role_play/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('games/', include('games.urls')),
+    path('game/', include('game.urls')),
     path('admin/', admin.site.urls),
 ]
