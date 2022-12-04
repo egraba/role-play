56dbf9f3328a669ebd2d4ec46f0e525610bf82cd
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-12-04 16:42:40 +0100

    Restrict game creation to name field

--- game/admin.py
+++ game/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 from .models import Game
 from .models import Character
 
-admin.site.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    fields = ['name']
+
+admin.site.register(Game, GameAdmin)
 admin.site.register(Character)
--- game/models.py
+++ game/models.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 from django.db import models
 
 class Game(models.Model):
-    start_date = models.DateTimeField()
+    start_date = models.DateTimeField(default=datetime.now())
     name = models.CharField(max_length=255, null=False, blank=False)
     end_date = models.DateTimeField(null=True, blank=True)
 
