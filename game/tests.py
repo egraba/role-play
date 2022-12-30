28063ed2b0fb34d784581a2701e110885a767cfb
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2022-12-30 15:56:42 +0100

    Implement first working unit test

--- game/tests.py
+++ game/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class IndexViewTests(TestCase):
+    def test_no_game(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No games are available...")
