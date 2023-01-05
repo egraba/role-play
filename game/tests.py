2de5c3de4dfab8195da1429dbc4f2bbfda9158ba
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-01-05 22:24:14 +0100

    Remove unnecessary parameter for objects creation

--- game/tests.py
+++ game/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 from django.urls import reverse
 
 from .models import Game
+from .models import Character
+from .models import Narrative
+from .models import PendingAction
 
 import random
 import string
@@ -13,10 +16,25 @@ def create_game():
     game_name = generate_random_string(random.randint(1, 255))
     return Game.objects.create(name=game_name)
 
-def create_several_games(n):
+def create_several_games():
     l = list()
+    n = random.randint(1, 100)
     for i in range(n):
         l.append(create_game())
+    return l
+
+def create_character(game):
+    return Character.objects.create(
+        name=generate_random_string(255),
+        game=game,
+        race=random.choice(Character.RACES)[0]
+    )
+
+def create_several_characters(game):
+    l = list()
+    n = random.randint(2, 10)
+    for i in range(n):
+        l.append(create_character(game))
     return l
 
 class IndexViewTests(TestCase):
@@ -26,7 +44,7 @@ class IndexViewTests(TestCase):
         self.assertContains(response, "No games are available...")
 
     def test_several_games(self):
-        game_list = create_several_games(random.randint(1, 100))
+        game_list = create_several_games()
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(
@@ -36,7 +54,7 @@ class IndexViewTests(TestCase):
 
 class GameViewTests(TestCase):
     def setUp(self):
-        create_game()
+        self.game = create_game()
         self.game_id = Game.objects.latest('start_date').pk
         
     def test_game_exists(self):
@@ -52,3 +70,12 @@ class GameViewTests(TestCase):
         response = self.client.get(reverse('game', args=[self.game_id]))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No characters for this game...")
+
+    def test_game_several_characters(self):
+        character_list = create_several_characters(self.game)
+        response = self.client.get(reverse('game', args=[self.game_id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            list(response.context['character_list']),
+            character_list,
+        )
