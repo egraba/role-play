09214dc3ff8745e533c334da2c661ed492937c67
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-03-24 19:36:12 +0100

    Add domain name in production settings

--- role_play/prod_settings.py
+++ role_play/prod_settings.py
@@ -28,13 +28,15 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://rpg.egraba.org/"]
 
 SECURE_SSL_REDIRECT = True
 
 SESSION_COOKIE_SECURE = True
 
 CSRF_COOKIE_SECURE = True
+
+CSRF_TRUSTED_ORIGINS = ["https://rpg.egraba.org/"]
 
 SECURE_HSTS_SECONDS = 3600
 
