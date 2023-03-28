351acb131a2c7c9890839453fd459ea8fca0fed1
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-03-28 19:01:13 +0200

    Prod fixes

--- role_play/prod_settings.py
+++ role_play/prod_settings.py
@@ -26,17 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["role-play-production.up.railway.app"]
 
-SECURE_SSL_REDIRECT = True
-
-SESSION_COOKIE_SECURE = True
-
-CSRF_COOKIE_SECURE = True
-
-CSRF_TRUSTED_ORIGINS = ["https://role-play-production.up.railway.app/"]
+CSRF_TRUSTED_ORIGINS = ["https://role-play-production.up.railway.app"]
 
 SECURE_HSTS_SECONDS = 3600
 
