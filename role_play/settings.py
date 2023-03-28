946e3efd0014410fcee64da5400444b343caaf63
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-03-28 19:32:17 +0200

    Add logging

--- role_play/settings.py
+++ role_play/settings.py
@@ -86,6 +86,12 @@ DATABASES = {
     }
 }
 
+# Authentication
+
+LOGIN_REDIRECT_URL = "/game"
+
+LOGOUT_REDIRECT_URL = "/game"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -127,7 +133,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-LOGIN_REDIRECT_URL = "/game"
-
-LOGOUT_REDIRECT_URL = "/game"
