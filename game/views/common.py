b69ade5a5e299cd9d3754c83e6ea14771c042369
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-04-04 22:44:26 +0200

    Pack imports

--- game/views/common.py
+++ game/views/common.py
@@ -2,12 +2,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import DetailView, ListView
 
-from game.models import Character, Event, Game, PendingAction, Tale
-from game.views.mixins import GameContextMixin
+import game.models as gmodels
+import game.views.mixins as gmixins
 
 
 class IndexView(ListView):
-    model = Game
+    model = gmodels.Game
     paginate_by = 5
     ordering = ["-start_date"]
     template_name = "game/index.html"
@@ -16,7 +16,7 @@ class IndexView(ListView):
         context = super().get_context_data(**kwargs)
         if self.request.user.is_authenticated:
             try:
-                context["character"] = Character.objects.filter(
+                context["character"] = gmodels.Character.objects.filter(
                     user=self.request.user
                 ).last()
             except ObjectDoesNotExist:
@@ -32,22 +32,22 @@ class IndexView(ListView):
             return super().get_queryset().none()
 
 
-class GameView(LoginRequiredMixin, ListView, GameContextMixin):
-    model = Event
+class GameView(LoginRequiredMixin, ListView, gmixins.GameContextMixin):
+    model = gmodels.Event
     paginate_by = 20
     ordering = ["-date"]
     template_name = "game/game.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["tale"] = Tale.objects.filter(game=self.game.id).last()
-        context["character_list"] = Character.objects.filter(
+        context["tale"] = gmodels.Tale.objects.filter(game=self.game.id).last()
+        context["character_list"] = gmodels.Character.objects.filter(
             game=self.game.id
         ).order_by("name")
         try:
-            player = Character.objects.get(user=self.request.user)
+            player = gmodels.Character.objects.get(user=self.request.user)
             context["player"] = player
-            context["pending_action"] = PendingAction.objects.filter(
+            context["pending_action"] = gmodels.PendingAction.objects.filter(
                 game=self.game.id, character=player
             ).get()
         except ObjectDoesNotExist:
@@ -59,5 +59,5 @@ class GameView(LoginRequiredMixin, ListView, GameContextMixin):
 
 
 class DetailCharacterView(DetailView):
-    model = Character
+    model = gmodels.Character
     template_name = "game/character.html"
