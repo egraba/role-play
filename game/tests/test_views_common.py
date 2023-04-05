159dbc969bea6d47408dcaf817e2d906d3dd5e4f
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-04-05 23:55:56 +0200

    Pack imports

--- game/tests/test_views_common.py
+++ game/tests/test_views_common.py
@@ -8,9 +8,9 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from game.models import Character, Event, Game, PendingAction, Tale
+import game.models as gmodels
+import game.views.common as gvcommon
 from game.tests import utils
-from game.views.common import DetailCharacterView, GameView, IndexView
 
 
 class IndexViewTest(TestCase):
@@ -21,9 +21,10 @@ class IndexViewTest(TestCase):
         user.set_password("pwd")
         user.user_permissions.add(permission)
         user.save()
+
         number_of_games = 10
         for i in range(number_of_games):
-            game = Game.objects.create(
+            game = gmodels.Game.objects.create(
                 name=utils.generate_random_string(20),
                 start_date=datetime.now(tz=timezone.utc),
             )
@@ -34,7 +35,7 @@ class IndexViewTest(TestCase):
     def test_view_mapping(self):
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.resolver_match.func.view_class, IndexView)
+        self.assertEqual(response.resolver_match.func.view_class, gvcommon.IndexView)
 
     def test_template_mapping(self):
         response = self.client.get(reverse("index"))
@@ -43,6 +44,7 @@ class IndexViewTest(TestCase):
     def test_pagination_size(self):
         self.user = User.objects.last()
         self.client.login(username=self.user.username, password="pwd")
+
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
         self.assertTrue("is_paginated" in response.context)
@@ -52,6 +54,7 @@ class IndexViewTest(TestCase):
     def test_pagination_size_next_page(self):
         self.user = User.objects.last()
         self.client.login(username=self.user.username, password="pwd")
+
         response = self.client.get(reverse("index") + "?page=2")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("is_paginated" in response.context)
@@ -72,9 +75,10 @@ class IndexViewTest(TestCase):
     def test_context_data_master_logged(self):
         self.user = User.objects.last()
         self.client.login(username=self.user.username, password="pwd")
+
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
-        game_list = Game.objects.filter(user=self.user)
+        game_list = gmodels.Game.objects.filter(user=self.user)
         self.assertTrue(set(response.context["game_list"]).issubset(set(game_list)))
 
     def test_context_data_anonymous_user(self):
@@ -89,9 +93,10 @@ class IndexViewTest(TestCase):
         user.user_permissions.add(permission)
         user.save()
         self.client.login(username=user.username, password="pwd")
+
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
-        game_list = Game.objects.filter(character__user=user)
+        game_list = gmodels.Game.objects.filter(character__user=user)
         self.assertQuerysetEqual(response.context["game_list"], game_list)
 
     def test_context_data_player_logged_no_existing_character(self):
@@ -101,6 +106,7 @@ class IndexViewTest(TestCase):
         user.user_permissions.add(permission)
         user.save()
         self.client.login(username=user.username, password="pwd")
+
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
         with self.assertRaises(KeyError):
@@ -113,11 +119,12 @@ class IndexViewTest(TestCase):
         user.set_password("pwd")
         user.user_permissions.add(permission)
         user.save()
-        Character.objects.create(name=utils.generate_random_name(5), user=user)
+
+        gmodels.Character.objects.create(name=utils.generate_random_name(5), user=user)
         self.client.login(username=user.username, password="pwd")
         response = self.client.get(reverse("index"))
         self.assertEqual(response.status_code, 200)
-        character = Character.objects.last()
+        character = gmodels.Character.objects.last()
         self.assertEqual(response.context["character"], character)
 
 
@@ -127,30 +134,31 @@ class GameViewTest(TestCase):
         user = User.objects.create(username=utils.generate_random_name(5))
         user.set_password("pwd")
         user.save()
-        game = Game.objects.create(name="game1")
+
+        game = gmodels.Game.objects.create(name="game1")
         number_of_events = 22
         for i in range(number_of_events):
-            Event.objects.create(
+            gmodels.Event.objects.create(
                 game=game,
                 message=f"{game.name} event{i}",
             )
         number_of_tales = 3
         for i in range(number_of_tales):
-            Tale.objects.create(
+            gmodels.Tale.objects.create(
                 game=game,
                 description=f"{game.name} tail{i}",
             )
         number_of_characters = 2
         for i in range(number_of_characters):
-            character = Character.objects.create(
+            character = gmodels.Character.objects.create(
                 game=game,
                 name=f"{game.name} character{i}",
-                race=random.choice(Character.RACES)[0],
+                race=random.choice(gmodels.Character.RACES)[0],
             )
-            PendingAction.objects.create(
+            gmodels.PendingAction.objects.create(
                 game=game,
                 character=character,
-                action_type=random.choice(PendingAction.ACTION_TYPES)[0],
+                action_type=random.choice(gmodels.PendingAction.ACTION_TYPES)[0],
                 message=f"{game.name} pending_action{i}",
             )
 
@@ -159,17 +167,17 @@ class GameViewTest(TestCase):
         self.client.login(username=self.user.username, password="pwd")
 
     def test_view_mapping(self):
-        game = Game.objects.last()
+        game = gmodels.Game.objects.last()
         response = self.client.get(reverse("game", args=[game.id]))
-        self.assertEqual(response.resolver_match.func.view_class, GameView)
+        self.assertEqual(response.resolver_match.func.view_class, gvcommon.GameView)
 
     def test_template_mapping(self):
-        game = Game.objects.last()
+        game = gmodels.Game.objects.last()
         response = self.client.get(reverse("game", args=[game.id]))
         self.assertTemplateUsed(response, "game/game.html")
 
     def test_pagination_size(self):
-        game = Game.objects.last()
+        game = gmodels.Game.objects.last()
         response = self.client.get(reverse("game", args=[game.id]))
         self.assertEqual(response.status_code, 200)
         self.assertTrue("is_paginated" in response.context)
@@ -177,7 +185,7 @@ class GameViewTest(TestCase):
         self.assertEqual(len(response.context["event_list"]), 20)
 
     def test_pagination_size_next_page(self):
-        game = Game.objects.last()
+        game = gmodels.Game.objects.last()
         response = self.client.get(reverse("game", args=[game.id]) + "?page=2")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("is_paginated" in response.context)
@@ -185,7 +193,7 @@ class GameViewTest(TestCase):
         self.assertEqual(len(response.context["event_list"]), 7)  # Inherited events
 
     def test_ordering_character_name_ascending(self):
-        game = Game.objects.last()
+        game = gmodels.Game.objects.last()
         response = self.client.get(reverse("game", args=[game.id]))
         self.assertEqual(response.status_code, 200)
         last_name = ""
@@ -197,7 +205,7 @@ class GameViewTest(TestCase):
                 last_name = character.name
 
     def test_ordering_event_date_descending(self):
-        game = Game.objects.last()
+        game = gmodels.Game.objects.last()
         response = self.client.get(reverse("game", args=[game.id]))
         self.assertEqual(response.status_code, 200)
         last_date = 0
@@ -215,8 +223,8 @@ class GameViewTest(TestCase):
         self.assertRaises(Http404)
 
     def test_game_last_tale(self):
-        game = Game.objects.last()
-        tale = Tale.objects.create(game=game)
+        game = gmodels.Game.objects.last()
+        tale = gmodels.Tale.objects.create(game=game)
         response = self.client.get(reverse("game", args=[game.id]))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["tale"], tale)
@@ -224,24 +232,26 @@ class GameViewTest(TestCase):
     def test_context_data_master(self):
         number_of_games = 3
         for i in range(number_of_games):
-            game = Game.objects.create(name=f"other_game{i}")
-            Tale.objects.create(game=game)
-            character = Character.objects.create(game=game, name=f"character{i}")
-            Event.objects.create(game=game)
-            PendingAction.objects.create(game=game, character=character)
+            game = gmodels.Game.objects.create(name=f"other_game{i}")
+            gmodels.Tale.objects.create(game=game)
+            character = gmodels.Character.objects.create(
+                game=game, name=f"character{i}"
+            )
+            gmodels.Event.objects.create(game=game)
+            gmodels.PendingAction.objects.create(game=game, character=character)
 
-        game = Game.objects.filter(name="game1").last()
+        game = gmodels.Game.objects.filter(name="game1").last()
         response = self.client.get(reverse("game", args=[game.id]))
         self.assertEqual(response.status_code, 200)
 
-        tale_list = Tale.objects.filter(game__name="game1")
+        tale_list = gmodels.Tale.objects.filter(game__name="game1")
         tale = tale_list.last()
         self.assertEqual(response.context["tale"], tale)
-        character_list = Character.objects.filter(game__name="game1")
+        character_list = gmodels.Character.objects.filter(game__name="game1")
         self.assertQuerysetEqual(
             list(response.context["character_list"]), character_list
         )
-        event_list = Event.objects.filter(game__name="game1")
+        event_list = gmodels.Event.objects.filter(game__name="game1")
         self.assertTrue(
             set(response.context["event_list"]).issubset(set(event_list))
         )  # issubset() is used because of pagination.
@@ -253,39 +263,41 @@ class GameViewTest(TestCase):
     def test_context_data_player(self):
         number_of_games = 3
         for i in range(number_of_games):
-            game = Game.objects.create(name=f"other_game{i}")
-            Tale.objects.create(game=game)
-            character = Character.objects.create(game=game, name=f"character{i}")
-            Event.objects.create(game=game)
-            PendingAction.objects.create(game=game, character=character)
+            game = gmodels.Game.objects.create(name=f"other_game{i}")
+            gmodels.Tale.objects.create(game=game)
+            character = gmodels.Character.objects.create(
+                game=game, name=f"character{i}"
+            )
+            gmodels.Event.objects.create(game=game)
+            gmodels.PendingAction.objects.create(game=game, character=character)
 
-        player = Character.objects.filter(name="game1 character1").get()
+        player = gmodels.Character.objects.filter(name="game1 character1").get()
         player.user = self.user
         player.save()
         self.client.logout()
         self.client.login(username=self.user.username, password="pwd")
 
-        game = Game.objects.filter(name="game1").last()
+        game = gmodels.Game.objects.filter(name="game1").last()
         response = self.client.get(reverse("game", args=[game.id]))
         self.assertEqual(response.status_code, 200)
 
-        tale_list = Tale.objects.filter(game__name="game1")
+        tale_list = gmodels.Tale.objects.filter(game__name="game1")
         tale = tale_list.last()
         self.assertEqual(response.context["tale"], tale)
-        character_list = Character.objects.filter(game__name="game1")
+        character_list = gmodels.Character.objects.filter(game__name="game1")
         self.assertQuerysetEqual(
             list(response.context["character_list"]), character_list
         )
-        event_list = Event.objects.filter(game__name="game1")
+        event_list = gmodels.Event.objects.filter(game__name="game1")
         self.assertTrue(
             set(response.context["event_list"]).issubset(set(event_list))
         )  # issubset() is used because of pagination.
         self.assertEqual(response.context["player"], player)
-        pending_action = PendingAction.objects.filter(
+        pending_action = gmodels.PendingAction.objects.filter(
             game__name="game1", character__name="game1 character1"
         ).get()
         self.assertEqual(response.context["pending_action"], pending_action)
-        PendingAction.objects.filter(
+        gmodels.PendingAction.objects.filter(
             game__name="game1", character__name="game1 character1"
         ).delete()
         response = self.client.get(reverse("game", args=[game.id]))
@@ -296,14 +308,16 @@ class GameViewTest(TestCase):
 class CharacterViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        Character.objects.create()
+        gmodels.Character.objects.create()
 
     def test_view_mapping(self):
-        character = Character.objects.last()
+        character = gmodels.Character.objects.last()
         response = self.client.get(reverse("character-detail", args=[character.id]))
-        self.assertEqual(response.resolver_match.func.view_class, DetailCharacterView)
+        self.assertEqual(
+            response.resolver_match.func.view_class, gvcommon.DetailCharacterView
+        )
 
     def test_template_mapping(self):
-        character = Character.objects.last()
+        character = gmodels.Character.objects.last()
         response = self.client.get(reverse("character-detail", args=[character.id]))
         self.assertTemplateUsed(response, "game/character.html")
