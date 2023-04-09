eeea12d0aca12ee3fa4c899ec3aca999bb98cfb2
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-04-09 19:16:19 +0200

    Update and fix email settings

--- role_play/prod_settings.py
+++ role_play/prod_settings.py
@@ -126,6 +126,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Email backend
 
+DEFAULT_FROM_EMAIL = os.environ["DEFAULT_FROM_EMAIL"]
+
 EMAIL_HOST = os.environ["EMAIL_HOST"]
 
 EMAIL_PORT = os.environ["EMAIL_PORT"]
@@ -134,7 +136,9 @@ EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 
-EMAIL_USE_SSL = os.environ["EMAIL_USE_SSL"]
+EMAIL_USE_TLS = True
+
+EMAIL_TIMEOUT = 10
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
