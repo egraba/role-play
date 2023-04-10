d6568c9b79defa8ff3b514c78142b60af745b7a0
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-04-10 14:13:52 +0200

    Add room model

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+import game.models as gmodels
+
+
+class Room(models.Model):
+    game = models.OneToOneField(gmodels.Game, on_delete=models.CASCADE)
+
 
 class Room(models.Model):
     game = models.OneToOneField(gmodels.Game, on_delete=models.CASCADE)
