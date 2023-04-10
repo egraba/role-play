29d13867d16540e0008c67b672b55c9f8710f065
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-04-10 18:45:32 +0200

    A room is now accessible from a game

--- chat/admin.py
+++ chat/admin.py
@@ -3,9 +3,14 @@ from django.contrib import admin
 import chat.models as cmodels
 
 
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ["game"]
+
+
 class MessageAdmin(admin.ModelAdmin):
     fields = ["date", "user", "content"]
     list_display = fields
 
 
+admin.site.register(cmodels.Room, RoomAdmin)
 admin.site.register(cmodels.Message, MessageAdmin)
