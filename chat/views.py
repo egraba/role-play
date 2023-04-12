a0af530d32024e52c702c350bfb5582118f5f2ce
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-04-12 21:15:37 +0200

    It is now possible to switch from chat to game

--- chat/views.py
+++ chat/views.py
@@ -6,3 +6,9 @@ import chat.models as cmodels
 class RoomView(DetailView):
     model = cmodels.Room
     template_name = "chat/room.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        room = self.object
+        context["message_list"] = cmodels.Message.objects.filter(room=room.id)
+        return context
