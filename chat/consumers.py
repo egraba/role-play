26638af42b1b729deb7c54c97bd338b3ccbef0de
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-04-13 00:57:45 +0200

    Fix context

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,10 +2,12 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.utils import timezone
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope["user"]
         self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
         self.room_group_name = f"chat_{self.room_id}"
         async_to_sync(self.channel_layer.group_add)(
@@ -21,14 +23,16 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        now = timezone.now()
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 "type": "chat_message",
                 "message": message,
+                "user": self.user.username,
+                "datetime": now.isoformat(),
             },
         )
 
     def chat_message(self, event):
-        message = event["message"]
-        self.send(text_data=json.dumps({"message": message}))
+        self.send(text_data=json.dumps(event))
