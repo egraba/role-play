4d29304425e8a0500047ed3e35a7b8c686918f6a
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-04-13 00:50:46 +0200

    Add username and date in the chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -22,7 +22,11 @@ class ChatConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat_message", "message": message}
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message,
+            },
         )
 
     def chat_message(self, event):
