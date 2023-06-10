720570501544d5d389f318cc87c6a31a7a06667e
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-06-10 17:22:04 +0200

    Fix daphne launch

--- role_play/asgi.py
+++ role_play/asgi.py
@@ -14,13 +14,14 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-import chat.routing as crouting
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "role_play.settings")
+django_asgi_app = get_asgi_application()
+
+import chat.routing as crouting
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(crouting.websocket_urlpatterns))
         ),
