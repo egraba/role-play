05adc52ecfb33909b1247f779d108cf3a0732fac
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-06-10 15:20:46 +0200

    Harmonize code

--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,6 @@
-# chat/routing.py
 from django.urls import re_path
 
-from . import consumers
+from chat import consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/chat/(?P<room_id>\w+)/$", consumers.ChatConsumer.as_asgi()),
