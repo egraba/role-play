17a68fe8db7fb5be7419267e68719a65200eb399
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-07-02 19:53:02 +0200

    Add test for get_master_email()

--- game/tests/test_utils.py
+++ game/tests/test_utils.py
@@ -1,17 +1,91 @@
 import os
 
+from django.contrib.auth.models import User
 from django.test import TestCase
 from email_validator import validate_email
 
+import game.models as gmodels
 import game.utils as gutils
 from game.tests import utils
 
 
 class GetMasterEmailTest(TestCase):
-    def test_valid_username(self):
+    def test_valid_username_format(self):
         username = utils.generate_random_name(20)
         email_domain = os.environ["EMAIL_DOMAIN"]
         expected_master_email = f"{username}@{email_domain}"
         master_email = gutils.get_master_email(username)
         self.assertEqual(master_email, expected_master_email)
         self.assertTrue(validate_email(master_email))
+
+    def test_invalid_username_format(self):
+        username = utils.generate_random_string(20)
+        master_email = gutils.get_master_email(username)
+        self.assertIsNone(master_email)
+
+
+class GetPlayersEmailsTest(TestCase):
+    def test_all_users_have_email(self):
+        game = gmodels.Game.objects.create(name=utils.generate_random_name(30))
+        emails = set()
+        for i in range(5):
+            email = (
+                utils.generate_random_name(5)
+                + "@"
+                + utils.generate_random_name(5)
+                + ".com"
+            )
+            emails.add(email)
+            user = User.objects.create(username=f"user{i}", email=email)
+            gmodels.Character.objects.create(name=f"character{i}", user=user, game=game)
+        self.assertEqual(gutils.get_players_emails(game), emails)
+
+    def test_some_users_missing(self):
+        game = gmodels.Game.objects.create(name=utils.generate_random_name(30))
+        emails = set()
+        for i in range(5):
+            if i % 2 == 0:
+                email = (
+                    utils.generate_random_name(5)
+                    + "@"
+                    + utils.generate_random_name(5)
+                    + ".com"
+                )
+                emails.add(email)
+                user = User.objects.create(username=f"user{i}", email=email)
+                gmodels.Character.objects.create(
+                    name=f"character{i}", game=game, user=user
+                )
+            else:
+                gmodels.Character.objects.create(name=f"character{i}", game=game)
+        self.assertEqual(gutils.get_players_emails(game), emails)
+
+    def test_some_emails_missing(self):
+        game = gmodels.Game.objects.create(name=utils.generate_random_name(30))
+        emails = set()
+        for i in range(5):
+            if i % 2 == 0:
+                email = (
+                    utils.generate_random_name(5)
+                    + "@"
+                    + utils.generate_random_name(5)
+                    + ".com"
+                )
+                emails.add(email)
+                user = User.objects.create(username=f"user{i}", email=email)
+            else:
+                user = User.objects.create(username=f"user{i}")
+            gmodels.Character.objects.create(name=f"character{i}", game=game, user=user)
+        self.assertEqual(gutils.get_players_emails(game), emails)
+
+    def test_same_emails(self):
+        game = gmodels.Game.objects.create(name=utils.generate_random_name(30))
+        emails = set()
+        email = (
+            utils.generate_random_name(5) + "@" + utils.generate_random_name(5) + ".com"
+        )
+        emails.add(email)
+        for i in range(5):
+            user = User.objects.create(username=f"user{i}", email=email)
+            gmodels.Character.objects.create(name=f"character{i}", user=user, game=game)
+        self.assertEqual(gutils.get_players_emails(game), emails)
--- game/utils.py
+++ game/utils.py
@@ -1,14 +1,24 @@
 import os
 
+from email_validator import EmailNotValidError, validate_email
+
 import game.models as gmodels
 
 
 def get_master_email(username):
     email_domain = os.environ["EMAIL_DOMAIN"]
     email = f"{username}@{email_domain}"
+    try:
+        validate_email(email)
+    except EmailNotValidError:
+        email = None
     return email
 
 
 def get_players_emails(game):
     players = gmodels.Character.objects.filter(game=game)
-    return [player.user.email for player in players if player.user is not None]
+    return {
+        player.user.email
+        for player in players
+        if player.user is not None and player.user.email != ""
+    }
