f5ace8ddcca6c5ed530f72c1c07c82eaf4ca4af5
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-07-09 16:20:34 +0200

    Add help text

--- master/models.py
+++ master/models.py
@@ -6,9 +6,22 @@ from django.utils.text import slugify
 class Story(models.Model):
     title = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, unique=True)
-    synopsis = models.TextField(max_length=3000)
-    main_conflict = models.TextField(max_length=1000)
-    objective = models.TextField(max_length=500)
+    synopsis = models.TextField(
+        max_length=3000, help_text="This is the background story of your game."
+    )
+    main_conflict = models.TextField(
+        max_length=1000,
+        help_text="""The conflict could be a person, such as a villain, or an event,
+        such as a natural disaster or viral disease.
+        The conflict will help to provide the objective of the game.""",
+    )
+    objective = models.TextField(
+        max_length=500,
+        help_text="""This needs to be clearly outlined in the rules so that all the players
+        understand what the main objective of the game is.
+        Popular win objectives include reaching a certain number of points,
+        achieving an objective, or reaching a certain point in the map.""",
+    )
 
     class Meta:
         verbose_name_plural = "stories"
