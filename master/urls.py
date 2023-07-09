781e804839473f3fefb3f2f275f3553305869b19
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-07-09 17:05:41 +0200

    Add ListStoryView

--- master/urls.py
+++ master/urls.py
@@ -4,6 +4,7 @@ import master.views as mviews
 
 urlpatterns = [
     path("story/<slug:slug>/", mviews.DetailStoryView.as_view(), name="story-detail"),
+    path("stories/", mviews.ListStoryView.as_view(), name="story-list"),
     path("story", mviews.CreateStoryView.as_view(), name="story-create"),
     path(
         "story/<slug:slug>/update",
--- master/views.py
+++ master/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 import master.forms as mforms
 import master.models as mmodels
@@ -7,6 +7,12 @@ import master.models as mmodels
 class DetailStoryView(DetailView):
     model = mmodels.Story
     template_name = "master/story.html"
+
+
+class ListStoryView(ListView):
+    model = mmodels.Story
+    paginate_by = 20
+    template_name = "master/storylist.html"
 
 
 class CreateStoryView(CreateView):
