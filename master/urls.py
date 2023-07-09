906bfc6c6b2b604c522ba958d33d1ab890412699
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-07-09 16:45:00 +0200

    It is now possible to edit a story

--- master/urls.py
+++ master/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 import master.views as mviews
 
 urlpatterns = [
-    path("story/<slug:slug>", mviews.DetailStoryView.as_view(), name="story-detail"),
+    path("story/<slug:slug>/", mviews.DetailStoryView.as_view(), name="story-detail"),
     path("story", mviews.CreateStoryView.as_view(), name="story-create"),
+    path(
+        "story/<slug:slug>/update",
+        mviews.UpdateStoryView.as_view(),
+        name="story-update",
+    ),
 ]
--- master/views.py
+++ master/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 import master.forms as mforms
 import master.models as mmodels
@@ -12,3 +12,9 @@ class DetailStoryView(DetailView):
 class CreateStoryView(CreateView):
     form_class = mforms.CreateStoryForm
     template_name = "master/createstory.html"
+
+
+class UpdateStoryView(UpdateView):
+    model = mmodels.Story
+    fields = ["synopsis", "main_conflict", "objective"]
+    template_name = "master/updatestory.html"
