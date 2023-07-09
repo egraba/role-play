5a9cb5d9db6545b5eb05fbc1d2b85be56cead7f3
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-07-09 15:10:09 +0200

    Refactor tests

--- master/tests/test_models.py
+++ master/tests/test_models.py
@@ -9,56 +9,48 @@ class StoryModelTest(TestCase):
     def setUpTestData(cls):
         mmodels.Story.objects.create()
 
+    def setUp(self):
+        self.story = mmodels.Story.objects.last()
+
     def test_title_type(self):
-        story = mmodels.Story.objects.last()
-        title = story._meta.get_field("title")
+        title = self.story._meta.get_field("title")
         self.assertTrue(title, models.CharField)
 
     def test_title_max_length(self):
-        story = mmodels.Story.objects.last()
-        max_length = story._meta.get_field("title").max_length
+        max_length = self.story._meta.get_field("title").max_length
         self.assertEqual(max_length, 50)
 
     def test_slug_type(self):
-        story = mmodels.Story.objects.last()
-        slug = story._meta.get_field("slug")
+        slug = self.story._meta.get_field("slug")
         self.assertTrue(slug, models.SlugField)
 
     def test_slug_max_length(self):
-        story = mmodels.Story.objects.last()
-        max_length = story._meta.get_field("slug").max_length
+        max_length = self.story._meta.get_field("slug").max_length
         self.assertEqual(max_length, 50)
 
     def test_synopsis_type(self):
-        story = mmodels.Story.objects.last()
-        synopsis = story._meta.get_field("synopsis")
+        synopsis = self.story._meta.get_field("synopsis")
         self.assertTrue(synopsis, models.TextField)
 
     def test_synopsis_max_length(self):
-        story = mmodels.Story.objects.last()
-        max_length = story._meta.get_field("synopsis").max_length
+        max_length = self.story._meta.get_field("synopsis").max_length
         self.assertEqual(max_length, 3000)
 
     def test_main_conflict_type(self):
-        story = mmodels.Story.objects.last()
-        main_conflict = story._meta.get_field("main_conflict")
+        main_conflict = self.story._meta.get_field("main_conflict")
         self.assertTrue(main_conflict, models.TextField)
 
     def test_main_conflict_max_length(self):
-        story = mmodels.Story.objects.last()
-        max_length = story._meta.get_field("main_conflict").max_length
+        max_length = self.story._meta.get_field("main_conflict").max_length
         self.assertEqual(max_length, 1000)
 
     def test_objective_type(self):
-        story = mmodels.Story.objects.last()
-        objective = story._meta.get_field("objective")
+        objective = self.story._meta.get_field("objective")
         self.assertTrue(objective, models.TextField)
 
     def test_objective_max_length(self):
-        story = mmodels.Story.objects.last()
-        max_length = story._meta.get_field("objective").max_length
+        max_length = self.story._meta.get_field("objective").max_length
         self.assertEqual(max_length, 500)
 
     def test_str(self):
-        story = mmodels.Story.objects.last()
-        self.assertEqual(str(story), story.title)
+        self.assertEqual(str(self.story), self.story.title)
