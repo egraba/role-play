24449cb29e13225f05eb89384a2c57a158b4a28c
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-07-11 20:16:16 +0200

    Fix form and utils tests

--- game/tests/test_utils.py
+++ game/tests/test_utils.py
@@ -4,9 +4,10 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from email_validator import validate_email
 
+import character.models as cmodels
 import game.models as gmodels
 import game.utils as gutils
-from game.tests import utils
+import utils.random as utils
 
 
 class GetMasterEmailTest(TestCase):
@@ -37,27 +38,8 @@ class GetPlayersEmailsTest(TestCase):
             )
             emails.add(email)
             user = User.objects.create(username=f"user{i}", email=email)
-            gmodels.Character.objects.create(name=f"character{i}", user=user, game=game)
-        self.assertEqual(gutils.get_players_emails(game), emails)
-
-    def test_some_users_missing(self):
-        game = gmodels.Game.objects.create(name=utils.generate_random_name(30))
-        emails = set()
-        for i in range(5):
-            if i % 2 == 0:
-                email = (
-                    utils.generate_random_name(5)
-                    + "@"
-                    + utils.generate_random_name(5)
-                    + ".com"
-                )
-                emails.add(email)
-                user = User.objects.create(username=f"user{i}", email=email)
-                gmodels.Character.objects.create(
-                    name=f"character{i}", game=game, user=user
-                )
-            else:
-                gmodels.Character.objects.create(name=f"character{i}", game=game)
+            character = cmodels.Character.objects.create(name=f"character{i}")
+            gmodels.Player.objects.create(game=game, character=character, user=user)
         self.assertEqual(gutils.get_players_emails(game), emails)
 
     def test_some_emails_missing(self):
@@ -73,9 +55,12 @@ class GetPlayersEmailsTest(TestCase):
                 )
                 emails.add(email)
                 user = User.objects.create(username=f"user{i}", email=email)
+                character = cmodels.Character.objects.create(name=f"character{i}")
+                gmodels.Player.objects.create(game=game, character=character, user=user)
             else:
                 user = User.objects.create(username=f"user{i}")
-            gmodels.Character.objects.create(name=f"character{i}", game=game, user=user)
+                character = cmodels.Character.objects.create(name=f"character{i}")
+                gmodels.Player.objects.create(game=game, character=character, user=user)
         self.assertEqual(gutils.get_players_emails(game), emails)
 
     def test_same_emails(self):
@@ -87,5 +72,6 @@ class GetPlayersEmailsTest(TestCase):
         emails.add(email)
         for i in range(5):
             user = User.objects.create(username=f"user{i}", email=email)
-            gmodels.Character.objects.create(name=f"character{i}", user=user, game=game)
+            character = cmodels.Character.objects.create(name=f"character{i}")
+            gmodels.Player.objects.create(game=game, character=character, user=user)
         self.assertEqual(gutils.get_players_emails(game), emails)
