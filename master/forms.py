39a85b4350345c9e243b8ca41c4e00a0a81eee29
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-07-14 10:08:08 +0200

    Remove useless method

--- master/forms.py
+++ master/forms.py
@@ -7,11 +7,3 @@ class CreateStoryForm(forms.ModelForm):
     class Meta:
         model = mmodels.Story
         exclude = ["slug"]
-
-    def clean_title(self):
-        title = self.cleaned_data["title"]
-        if mmodels.Story.objects.filter(title=title).exists():
-            raise forms.ValidationError(
-                "A story with the same title already exists... Please find another title."
-            )
-        return title
