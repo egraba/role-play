3ba358ce761b8196c05cb6ea8d39b59272ca84e5
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-07-19 17:32:35 +0200

    Simplify ChoiceViewTest

--- game/tests/test_views_player.py
+++ game/tests/test_views_player.py
@@ -276,38 +276,34 @@ class ChoiceViewTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        user = User.objects.create(username=utrandom.ascii_letters_string(5))
-        user.set_password("pwd")
-        user.save()
-
-        game = gmodels.Game.objects.create()
+        game = utfactories.GameFactory()
         # A game can only start with a minimum number of characters.
         number_of_players = 2
         for i in range(number_of_players):
-            character = cmodels.Character.objects.create(
-                name=utrandom.ascii_letters_string(5),
+            character = utfactories.CharacterFactory()
+            gmodels.Player.objects.create(
+                game=game, character=character, user=character.user
             )
-            gmodels.Player.objects.create(game=game, character=character)
-            gmodels.PendingAction.objects.create(game=game, character=character)
+            utfactories.PendingActionFactory(game=game, character=character)
         game.start()
         game.save()
 
     def setUp(self):
         self.user = User.objects.last()
         self.client.login(username=self.user.username, password="pwd")
+        self.game = gmodels.Game.objects.last()
+        self.character = cmodels.Character.objects.last()
 
     def tearDown(self):
         cache.clear()
 
     def test_view_mapping(self):
-        game = gmodels.Game.objects.last()
-        character = cmodels.Character.objects.last()
         response = self.client.get(
             reverse(
                 self.path_name,
                 args=(
-                    game.id,
-                    character.id,
+                    self.game.id,
+                    self.character.id,
                 ),
             )
         )
@@ -315,14 +311,12 @@ class ChoiceViewTest(TestCase):
         self.assertEqual(response.resolver_match.func.view_class, gvplayer.ChoiceView)
 
     def test_template_mapping(self):
-        game = gmodels.Game.objects.last()
-        character = cmodels.Character.objects.last()
         response = self.client.get(
             reverse(
                 self.path_name,
                 args=(
-                    game.id,
-                    character.id,
+                    self.game.id,
+                    self.character.id,
                 ),
             )
         )
@@ -331,13 +325,12 @@ class ChoiceViewTest(TestCase):
 
     def test_game_not_exists(self):
         game_id = random.randint(10000, 99999)
-        character = cmodels.Character.objects.last()
         response = self.client.get(
             reverse(
                 self.path_name,
                 args=(
                     game_id,
-                    character.id,
+                    self.character.id,
                 ),
             )
         )
@@ -345,13 +338,12 @@ class ChoiceViewTest(TestCase):
         self.assertRaises(Http404)
 
     def test_character_not_exists(self):
-        game = gmodels.Game.objects.last()
         character_id = random.randint(10000, 99999)
         response = self.client.get(
             reverse(
                 self.path_name,
                 args=(
-                    game.id,
+                    self.game.id,
                     character_id,
                 ),
             )
@@ -360,30 +352,28 @@ class ChoiceViewTest(TestCase):
         self.assertRaises(Http404)
 
     def test_context_data(self):
-        game = gmodels.Game.objects.last()
-        character = cmodels.Character.objects.last()
         response = self.client.get(
             reverse(
                 self.path_name,
                 args=(
-                    game.id,
-                    character.id,
+                    self.game.id,
+                    self.character.id,
                 ),
             )
         )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context["game"], game)
-        self.assertEqual(response.context["character"], character)
+        self.assertEqual(response.context["game"], self.game)
+        self.assertEqual(response.context["character"], self.character)
 
     def test_game_is_under_preparation(self):
-        game = gmodels.Game.objects.create(name=utrandom.printable_string(20))
-        character = cmodels.Character.objects.last()
+        self.game.status = "P"
+        self.game.save()
         response = self.client.get(
             reverse(
                 self.path_name,
                 args=(
-                    game.id,
-                    character.id,
+                    self.game.id,
+                    self.character.id,
                 ),
             )
         )
@@ -391,16 +381,14 @@ class ChoiceViewTest(TestCase):
         self.assertRaises(PermissionDenied)
 
     def test_game_is_finished(self):
-        game = gmodels.Game.objects.last()
-        character = cmodels.Character.objects.last()
-        game.end()
-        game.save()
+        self.game.end()
+        self.game.save()
         response = self.client.get(
             reverse(
                 self.path_name,
                 args=(
-                    game.id,
-                    character.id,
+                    self.game.id,
+                    self.character.id,
                 ),
             )
         )
@@ -413,28 +401,23 @@ class ChoiceViewTest(TestCase):
         form = gforms.ChoiceForm(data)
         self.assertTrue(form.is_valid())
 
-        game = gmodels.Game.objects.last()
-        character = cmodels.Character.objects.last()
         response = self.client.post(
             reverse(
                 self.path_name,
                 args=(
-                    game.id,
-                    character.id,
+                    self.game.id,
+                    self.character.id,
                 ),
             ),
             data=form.cleaned_data,
         )
         self.assertEqual(response.status_code, 302)
         choice = gmodels.Choice.objects.last()
-        self.assertRedirects(
-            response,
-            reverse("game", args=(game.id,)),
-        )
-        self.assertEqual(choice.game, game)
+        self.assertRedirects(response, self.game.get_absolute_url())
+        self.assertEqual(choice.game, self.game)
         self.assertLessEqual(choice.date.second - timezone.now().second, 2)
         self.assertEqual(
             choice.message,
-            f"{character} made a choice: {choice.selection}.",
+            f"{self.character} made a choice: {choice.selection}.",
         )
         self.assertEqual(choice.selection, form.cleaned_data["selection"])
