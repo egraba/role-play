2c9fcb23c58585391d4bfe819087cdd0eaa29602
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-07-25 23:20:04 +0200

    Add user in character admin

--- character/admin.py
+++ character/admin.py
@@ -4,8 +4,8 @@ import character.models as cmodels
 
 
 class CharacterAdmin(admin.ModelAdmin):
-    fields = ["name", "race"]
-    list_display = ["name", "race"]
+    fields = ["name", "user", "race"]
+    list_display = ["name", "user", "race"]
 
 
 admin.site.register(cmodels.Character, CharacterAdmin)
