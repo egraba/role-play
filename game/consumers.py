f7a0ed031e83a47c772bcb30cea6e0400dcd826a
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-08-06 13:01:44 +0200

    Fix format of input messages

--- game/consumers.py
+++ game/consumers.py
@@ -9,7 +9,6 @@ import game.models as gmodels
 
 class EventsConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.user = self.scope["user"]
         self.game_id = self.scope["url_route"]["kwargs"]["game_id"]
         self.game_group_name = f"events_{self.game_id}"
         await self.channel_layer.group_add(self.game_group_name, self.channel_name)
@@ -26,17 +25,15 @@ class EventsConsumer(AsyncWebsocketConsumer):
             self.game_group_name,
             {
                 "type": "game_event",
+                "date": now.isoformat(),
                 "message": message,
-                "user": self.user.username,
-                "datetime": now.isoformat(),
             },
         )
 
     async def game_event(self, event):
-        user = self.user
-        content = event["message"]
+        message = event["message"]
         game = await database_sync_to_async(gmodels.Game.objects.get)(id=self.game_id)
         await database_sync_to_async(gmodels.Event.objects.create)(
-            user=user, content=content, game=game
+            game=game, message=message
         )
         await self.send(text_data=json.dumps(event))
