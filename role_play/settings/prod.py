1c4c5faf40d8bed2e5f72273f71da3ad9e449b34
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-08-20 19:03:02 +0200

    Enhance structure of django projects settings

---
+++ role_play/settings/prod.py
@@ -0,0 +1,66 @@
+import json
+
+import dj_database_url
+
+from role_play.settings.base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = json.loads(os.environ["ALLOWED_HOSTS"])
+
+CSRF_TRUSTED_ORIGINS = json.loads(os.environ["CSRF_TRUSTED_ORIGINS"])
+
+SECURE_HSTS_SECONDS = 3600
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_HSTS_PRELOAD = True
+
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+
+WSGI_APPLICATION = "role_play.wsgi.application"
+
+
+# Email backend
+
+DEFAULT_FROM_EMAIL = os.environ["DEFAULT_FROM_EMAIL"]
+
+EMAIL_HOST = os.environ["EMAIL_HOST"]
+
+EMAIL_PORT = os.environ["EMAIL_PORT"]
+
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+
+EMAIL_USE_TLS = True
+
+EMAIL_TIMEOUT = 10
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+# Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
