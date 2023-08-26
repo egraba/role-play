ca21948766ee98eca83805b1ab35a940d8df29d7
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-08-26 18:21:04 +0200

    Add ability scores in form

--- character/forms.py
+++ character/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 import character.models as cmodels
 
+ability_scores = [(15, 15), (14, 14), (13, 13), (12, 12), (10, 10), (8, 8)]
+
 
 class CreateCharacterForm(forms.ModelForm):
     class Meta:
@@ -10,14 +12,39 @@ class CreateCharacterForm(forms.ModelForm):
             "name",
             "race",
             "class_name",
-            "strength",
-            "dexterity",
-            "constitution",
-            "intelligence",
-            "wisdom",
-            "charisma",
         ]
         widgets = {
             "race": forms.Select(attrs={"class": "rpgui-dropdown"}),
             "class_name": forms.Select(attrs={"class": "rpgui-dropdown"}),
         }
+
+    strength = forms.TypedChoiceField(
+        choices=ability_scores,
+        coerce=int,
+        widget=forms.Select(attrs={"class": "rpgui-dropdown"}),
+    )
+    dexterity = forms.TypedChoiceField(
+        choices=ability_scores,
+        coerce=int,
+        widget=forms.Select(attrs={"class": "rpgui-dropdown"}),
+    )
+    constitution = forms.TypedChoiceField(
+        choices=ability_scores,
+        coerce=int,
+        widget=forms.Select(attrs={"class": "rpgui-dropdown"}),
+    )
+    intelligence = forms.TypedChoiceField(
+        choices=ability_scores,
+        coerce=int,
+        widget=forms.Select(attrs={"class": "rpgui-dropdown"}),
+    )
+    wisdom = forms.TypedChoiceField(
+        choices=ability_scores,
+        coerce=int,
+        widget=forms.Select(attrs={"class": "rpgui-dropdown"}),
+    )
+    charisma = forms.TypedChoiceField(
+        choices=ability_scores,
+        coerce=int,
+        widget=forms.Select(attrs={"class": "rpgui-dropdown"}),
+    )
