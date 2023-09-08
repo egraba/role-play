d8233603ec9d6403bc12452f6d8d04efcdb1165e
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-09-08 18:59:44 +0200

    Add AbilityScoreIncrease model

--- character/models.py
+++ character/models.py
@@ -156,3 +156,17 @@ class RacialTrait(models.Model):
     speed = models.SmallIntegerField()
     languages = models.ManyToManyField(Language)
     abilities = models.ManyToManyField(Ability)
+
+
+class AbilityScoreIncrease(models.Model):
+    class Ability(models.TextChoices):
+        STRENGTH = "strength"
+        DEXTERITY = "dexterity"
+        CONSTITUTION = "constitution"
+        INTELLIGENCE = "intelligence"
+        WISDOM = "wisdom"
+        CHARISMA = "charisma"
+
+    racial_trait = models.ForeignKey(RacialTrait, on_delete=models.CASCADE)
+    ability = models.CharField(max_length=20, choices=Ability.choices)
+    increase = models.SmallIntegerField()
