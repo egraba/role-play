7e0ec302e5a82d94dce0ad9f35a933490416e7ca
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-09-10 18:11:45 +0200

    Create command to delete characters

---
+++ character/management/commands/deletecharacter.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.management.base import BaseCommand, CommandError
+
+import character.models as cmodels
+
+
+class Command(BaseCommand):
+    help = "delete a user's character"
+
+    def add_arguments(self, parser):
+        parser.add_argument("user", type=str, help="character's user")
+
+    def handle(self, *args, **options):
+        username = options["user"]
+        try:
+            user = User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            raise CommandError(f"[{username}] doesn't exist...")
+        try:
+            cmodels.Character.objects.filter(user=user).delete()
+        except ObjectDoesNotExist:
+            raise CommandError(f"[{username}] doesn't have a character...")
+
+        self.stdout.write(self.style.SUCCESS("Successfully deleted the character"))
