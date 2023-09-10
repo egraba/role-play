deb37d4542c7b68020a40af40a23ad9889cc516c
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-09-10 19:08:55 +0200

    Add ClassAdvancement in django admin site

--- character/admin.py
+++ character/admin.py
@@ -18,6 +18,13 @@ class AbilityAdmin(admin.ModelAdmin):
     list_display = ["name", "description"]
 
 
+class ClassAdvancementAdmin(admin.ModelAdmin):
+    fields = ["class_name", "level", "proficiency_bonus"]
+    list_display = ["class_name", "level", "proficiency_bonus"]
+    ordering = ["class_name", "level"]
+
+
 admin.site.register(cmodels.Character, CharacterAdmin)
 admin.site.register(cmodels.Advancement, AdvancementAdmin)
 admin.site.register(cmodels.Ability, AbilityAdmin)
+admin.site.register(cmodels.ClassAdvancement, ClassAdvancementAdmin)
