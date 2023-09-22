569b58b94bc29a75bb33d1832ad251aa21b6a8bc
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-09-22 19:49:10 +0200

    Add prod name space into tasks

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -2,6 +2,7 @@ from invoke import Collection
 
 import tasks.db as db
 import tasks.dev as dev
+import tasks.prod as prod
 import tasks.test as test
 
-namespace = Collection(dev, db, test)
+namespace = Collection(dev, db, test, prod)
