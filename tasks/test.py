869c1f7076be1a6e92df812c680376566577b570
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-09-22 18:19:52 +0200

    Add verbosity options

--- tasks/test.py
+++ tasks/test.py
@@ -1,11 +1,14 @@
 from invoke import task
 
 
-@task(help={"coverage": "Measure test coverage"})
-def run(c, coverage=False):
+@task(help={"coverage": "Measure test coverage", "verbose": "Increase verbosity"})
+def run(c, coverage=False, verbose=False):
     """Test the app"""
+    cmd_options = ""
+    if verbose:
+        cmd_options += "--verbosity=2"
     if coverage:
-        c.run("coverage run manage.py test")
+        c.run(f"coverage run manage.py test {cmd_options}")
         c.run("coverage html")
     else:
-        c.run("python manage.py test")
+        c.run(f"python manage.py test {cmd_options}")
