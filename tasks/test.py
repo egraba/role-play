1d8c7e30cca1b0db793e009f895c5ebe75f90ef8
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-09-22 20:29:14 +0200

    Add test_label as testing argument

--- tasks/test.py
+++ tasks/test.py
@@ -1,12 +1,20 @@
 from invoke import task
 
 
-@task(help={"coverage": "Measure test coverage", "verbose": "Increase verbosity"})
-def run(c, coverage=False, verbose=False):
+@task(
+    help={
+        "coverage": "Measure test coverage",
+        "verbose": "Increase verbosity",
+        "test_label": "Module paths to test (i.e Django test_label option)",
+    }
+)
+def run(c, coverage=False, verbose=False, test_label=None):
     """Test the app"""
     cmd_options = ""
     if verbose:
         cmd_options += "--verbosity=2"
+    if test_label:
+        cmd_options += test_label
     if coverage:
         c.run(f"coverage run manage.py test {cmd_options}")
         c.run("coverage html")
