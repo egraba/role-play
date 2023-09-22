c73ad594a4a1b79865b8f1d1c87dee73d3fe5c69
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-09-22 16:26:46 +0200

    Create minimal run task

--- tasks.py
+++ tasks.py
@@ -6,3 +6,8 @@ def clean(c):
     patterns = ["htmlcov", "staticfiles", ".coverage"]
     for pattern in patterns:
         c.run(f"rm -rf {pattern}")
+
+
+@task
+def run(c):
+    c.run("python manage.py runserver")
