0e64fcbe249661b9d53251bda06f9c5cf8bff220
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-09-30 12:56:15 +0200

    build: Celery (#141)
    * feat: Implement first Celery task
    
    * build: Add invoke task to run Celery workers
    
    * build: Add worker tasks to run Celery worker in production
    
    * style: Small fixes in email sending
    
    * refactor: All emails are now sent as tasks
    
    * style: Make emails parameters clearer
    
    * chore: Add colors for some invoke tasks
    
    * feat: Store master instruction via a Celery task
    
    * chore: Remove Celery warnings
    
    * feat: Store player choice via a Celery task
    
    * feat: Store player dice launch via a Celery task
    
    * docs(changelog): Update changelog
    
    * fix: Run celery worker on Railway

--- tasks/prod.py
+++ tasks/prod.py
@@ -4,6 +4,12 @@ import tasks.db as db
 
 
 @task
+def run_worker(c):
+    """Run Celery worker"""
+    c.run("celery -A role_play worker -l INFO", asynchronous=True)
+
+
+@task(run_worker)
 def build(c):
     """Apply database migrations"""
     c.run("poetry run python manage.py migrate")
