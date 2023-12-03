3327067ebfd2f59a7759d6fb2e3325ca80ed9654
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-12-03 19:52:28 +0100

    test: migrate dice tests to pytest

--- utils/test_dices.py
+++ utils/test_dices.py
@@ -1,31 +1,31 @@
-from unittest import TestCase
-
+import pytest
 from faker import Faker
 
 from utils.dices import Dice, DiceError, types
 
 
-class DiceTest(TestCase):
-    def test_constructor_valid_dice(self):
-        fake = Faker()
-        number_of_throws = fake.random_int(min=1, max=10)
-        dice_type = fake.random_element(elements=types)
-        dice_str = f"{number_of_throws}d{dice_type}"
-        dice = Dice(dice_str=dice_str)
-        self.assertEqual(dice.throws, number_of_throws)
-        self.assertEqual(dice.type, dice_type)
-        self.assertEqual(str(dice), dice_str)
+def test_constructor_valid_dice():
+    fake = Faker()
+    number_of_throws = fake.random_int(min=1, max=10)
+    dice_type = fake.random_element(elements=types)
+    dice_str = f"{number_of_throws}d{dice_type}"
+    dice = Dice(dice_str=dice_str)
+    assert dice.throws == number_of_throws
+    assert dice.type == dice_type
+    assert str(dice) == dice_str
 
-    def test_constructor_invalid_dice_str(self):
-        fake = Faker()
-        dice_str = fake.pystr(max_chars=5)
-        with self.assertRaises(TypeError):
-            Dice(dice_str)
 
-    def test_constructor_invalid_dice_type(self):
-        fake = Faker()
-        number_of_throws = fake.random_int(min=1, max=10)
-        dice_type = fake.random_element(elements=(2, 3, 5, 100))
-        dice_str = f"{number_of_throws}d{dice_type}"
-        with self.assertRaises(DiceError):
-            Dice(dice_str)
+def test_constructor_invalid_dice_str():
+    fake = Faker()
+    dice_str = fake.pystr(max_chars=5)
+    with pytest.raises(TypeError):
+        Dice(dice_str)
+
+
+def test_constructor_invalid_dice_type():
+    fake = Faker()
+    number_of_throws = fake.random_int(min=1, max=10)
+    dice_type = fake.random_element(elements=(2, 3, 5, 100))
+    dice_str = f"{number_of_throws}d{dice_type}"
+    with pytest.raises(DiceError):
+        Dice(dice_str)
