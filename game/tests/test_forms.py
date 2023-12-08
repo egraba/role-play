450c9e4b2dc0cba72c2c499c5e7bd12be74cd4de
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2023-12-08 13:52:07 +0100

    test(game): migrate forms tests

--- game/tests/test_forms.py
+++ game/tests/test_forms.py
@@ -1,57 +1,56 @@
 from django import forms
-from django.test import TestCase
 
 from character.forms.character import CreateCharacterForm
 from game.forms import ChoiceForm, CreateQuestForm, DamageForm, HealForm, IncreaseXpForm
 
 
-class CreateQuestFormTest(TestCase):
+class TestCreateQuestForm:
     def test_content_type(self):
         form = CreateQuestForm()
-        self.assertIsInstance(form.fields["content"].widget, forms.Textarea)
+        assert isinstance(form.fields["content"].widget, forms.Textarea)
 
     def test_content_max_length(self):
         form = CreateQuestForm()
-        self.assertEqual(form.fields["content"].max_length, 1000)
+        assert form.fields["content"].max_length == 1000
 
 
-class IncreaseXpFormTest(TestCase):
+class TestIncreaseXpForm:
     def test_xp_type(self):
         form = IncreaseXpForm()
-        self.assertIsInstance(form.fields["xp"].widget, forms.NumberInput)
+        assert isinstance(form.fields["xp"].widget, forms.NumberInput)
 
 
-class DamageFormTest(TestCase):
+class TestDamageForm:
     def test_hp_type(self):
         form = DamageForm()
-        self.assertIsInstance(form.fields["hp"].widget, forms.NumberInput)
+        assert isinstance(form.fields["hp"].widget, forms.NumberInput)
 
 
-class HealFormTest(TestCase):
+class TestHealForm:
     def test_hp_type(self):
         form = HealForm()
-        self.assertIsInstance(form.fields["hp"].widget, forms.NumberInput)
+        assert isinstance(form.fields["hp"].widget, forms.NumberInput)
 
 
-class CreateCharacterFormTest(TestCase):
+class TestCreateCharacterForm:
     def test_name_type(self):
         form = CreateCharacterForm()
-        self.assertIsInstance(form.fields["name"].widget, forms.TextInput)
+        assert isinstance(form.fields["name"].widget, forms.TextInput)
 
     def test_name_max_length(self):
         form = CreateCharacterForm()
-        self.assertEqual(form.fields["name"].max_length, 100)
+        assert form.fields["name"].max_length == 100
 
     def test_race_type(self):
         form = CreateCharacterForm()
-        self.assertIsInstance(form.fields["race"].widget, forms.Select)
+        assert isinstance(form.fields["race"].widget, forms.Select)
 
 
-class ChoiceFormTest(TestCase):
+class TestChoiceForm:
     def test_selection_type(self):
         form = ChoiceForm()
-        self.assertIsInstance(form.fields["selection"].widget, forms.Textarea)
+        assert isinstance(form.fields["selection"].widget, forms.Textarea)
 
     def test_name_max_length(self):
         form = ChoiceForm()
-        self.assertEqual(form.fields["selection"].max_length, 50)
+        assert form.fields["selection"].max_length == 50
