fea1e8b5e4f32ced2ac1ef830d8d20cb04f9e09e
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-01-02 21:11:39 +0100

    refactor(utils): use subclass of str type instead of functions

--- utils/dice.py
+++ utils/dice.py
@@ -11,29 +11,26 @@ class DiceFormatError(TypeError):
     pass
 
 
-def add_throws(dice_str, throws):
-    """Add throws to a dice string.
+class Dice(str):
+    def add_throws(self, throws):
+        """Add throws to a dice string.
 
-    A dice string looks like 'NdT', where N is the number of throws
-    and T the type of the dice.
-    This function adds the throws passed as parameter to N.
+        A dice string looks like 'NdT', where N is the number of throws
+        and T the type of the dice.
+        This method adds the throws passed as parameter to N.
 
-    Args:
-        dice_str (str): Dice string.
-        throws (int): Number of throws to add to the dice string.
+        Args:
+            dice_str (str): Dice string.
 
-    Returns:
-        str: New dice string with added throws.
+        """
 
-    """
+        if not re.match(DICE_REGEX, self):
+            raise DiceFormatError(f"[{self}] does not match a dice regex...")
+        dice_str_parts = self.split("d")
+        dice_type = int(dice_str_parts[1])
+        if dice_type not in types:
+            raise DiceFormatError("The provided dice type is not supported...")
 
-    if not re.match(DICE_REGEX, dice_str):
-        raise DiceFormatError(f"[{dice_str}] does not match a dice regex...")
-    dice_str_parts = dice_str.split("d")
-    dice_type = int(dice_str_parts[1])
-    if dice_type not in types:
-        raise DiceFormatError("The provided dice type is not supported...")
-
-    dice_str_parts[0] = str(int(dice_str_parts[0]) + throws)
-    dice_str = "d".join(dice_str_parts)
-    return dice_str
+        dice_str_parts[0] = str(int(dice_str_parts[0]) + throws)
+        self = "d".join(dice_str_parts)
+        return self
