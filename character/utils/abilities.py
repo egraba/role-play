631ca18289ebe688bfcdca548ea275c79f0f2997
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-01-05 18:24:35 +0100

    refactor: Feature/improve abilities utils (#162)
    * fix(character): fix display of ability scores in form
    
    * refactor(utils): extract and improve choice converter (#161)
    
    * style(character): add comments

--- character/utils/abilities.py
+++ character/utils/abilities.py
@@ -1,14 +1,27 @@
-from django.db import models
+from django.db.models import IntegerChoices
 
 
-class AbilityScore(models.IntegerChoices):
-    SCORE_15 = 15
-    SCORE_14 = 14
-    SCORE_13 = 13
-    SCORE_12 = 12
-    SCORE_10 = 10
-    SCORE_8 = 8
+class AbilityScore(IntegerChoices):
+    SCORE_15 = 15, "15"
+    SCORE_14 = 14, "14"
+    SCORE_13 = 13, "13"
+    SCORE_12 = 12, "12"
+    SCORE_10 = 10, "10"
+    SCORE_8 = 8, "8"
 
 
-def compute_ability_modifier(ability_score):
+def compute_ability_modifier(ability_score: int) -> int:
+    """Compute ability modifier.
+
+    The bigger ability score is, the bigger the modifier is, and vice versa.
+    The formula is given by DnD rules.
+
+    Args:
+        ability_score (int): Ability score.
+
+    Returns:
+        int: The modifier corresponding to the score given as parameter.
+
+    """
+
     return (ability_score - 10) // 2
