9fc4977b33c547d5cafd85e192adec082643987d
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-01-05 18:22:18 +0100

    style(character): add comments

--- character/utils/abilities.py
+++ character/utils/abilities.py
@@ -10,5 +10,18 @@ class AbilityScore(IntegerChoices):
     SCORE_8 = 8, "8"
 
 
-def compute_ability_modifier(ability_score):
+def compute_ability_modifier(ability_score: int) -> int:
+    """Compute ability modifier.
+
+    The bigger ability score is, the bigger the modifier is, and vice versa.
+    The formula is given by DnD rules.
+
+    Args:
+        ability_score (int): Ability score.
+
+    Returns:
+        int: The modifier corresponding to the score given as parameter.
+
+    """
+
     return (ability_score - 10) // 2
