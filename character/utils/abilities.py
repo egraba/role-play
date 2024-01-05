b8e865720ad40d06ec0d7a3828866e4e2da5f5a5
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-01-05 18:11:16 +0100

    fix(character): fix display of ability scores in form

--- character/utils/abilities.py
+++ character/utils/abilities.py
@@ -1,13 +1,13 @@
-from django.db import models
+from django.db.models import IntegerChoices
 
 
-class AbilityScore(models.IntegerChoices):
-    SCORE_15 = 15
-    SCORE_14 = 14
-    SCORE_13 = 13
-    SCORE_12 = 12
-    SCORE_10 = 10
-    SCORE_8 = 8
+class AbilityScore(IntegerChoices):
+    SCORE_15 = 15, "15"
+    SCORE_14 = 14, "14"
+    SCORE_13 = 13, "13"
+    SCORE_12 = 12, "12"
+    SCORE_10 = 10, "10"
+    SCORE_8 = 8, "8"
 
 
 def compute_ability_modifier(ability_score):
