4c44132ab7526213fc0451d5aba5ccca393e3555
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-01-06 09:40:50 +0100

    test(character): use autouse when necessary

--- character/tests/forms/test_equipment_selection.py
+++ character/tests/forms/test_equipment_selection.py
@@ -13,38 +13,38 @@ from character.forms.equipment_selection import (
 class TestClericEquipmentSelectForm:
     form = None
 
-    @pytest.fixture
+    @pytest.fixture(autouse=True)
     def cleric(self):
         self.form = ClericEquipmentSelectForm()
 
-    def test_first_weapon_field_exists_for_cleric(self, cleric):
+    def test_first_weapon_field_exists_for_cleric(self):
         assert "first_weapon" in self.form.fields
 
-    def test_first_weapon_field_type_for_cleric(self, cleric):
+    def test_first_weapon_field_type_for_cleric(self):
         assert isinstance(self.form.fields["first_weapon"], forms.ChoiceField)
 
-    def test_amor_field_exists_for_cleric(self, cleric):
+    def test_amor_field_exists_for_cleric(self):
         assert "armor" in self.form.fields
 
-    def test_armor_field_type_for_cleric(self, cleric):
+    def test_armor_field_type_for_cleric(self):
         assert isinstance(self.form.fields["armor"], forms.ChoiceField)
 
-    def test_second_weapon_field_exists_for_cleric(self, cleric):
+    def test_second_weapon_field_exists_for_cleric(self):
         assert "second_weapon" in self.form.fields
 
-    def test_second_weapon_field_type_for_cleric(self, cleric):
+    def test_second_weapon_field_type_for_cleric(self):
         assert isinstance(self.form.fields["second_weapon"], forms.ChoiceField)
 
-    def test_third_weapon_field_does_not_exist_for_cleric(self, cleric):
+    def test_third_weapon_field_does_not_exist_for_cleric(self):
         assert "third_weapon" not in self.form.fields
 
-    def test_pack_field_exists_for_cleric(self, cleric):
+    def test_pack_field_exists_for_cleric(self):
         assert "pack" in self.form.fields
 
-    def test_pack_field_type_for_cleric(self, cleric):
+    def test_pack_field_type_for_cleric(self):
         assert isinstance(self.form.fields["pack"], forms.ChoiceField)
 
-    def test_field_order(self, cleric):
+    def test_field_order(self):
         assert list(self.form.fields.keys()) == [
             "first_weapon",
             "second_weapon",
@@ -58,38 +58,38 @@ class TestClericEquipmentSelectForm:
 class TestFighterEquipmentSelectForm:
     form = None
 
-    @pytest.fixture
+    @pytest.fixture(autouse=True)
     def fighter(self):
         self.form = FighterEquipmentSelectForm()
 
-    def test_first_weapon_field_exists_for_fighter(self, fighter):
+    def test_first_weapon_field_exists_for_fighter(self):
         assert "first_weapon" in self.form.fields
 
-    def test_first_weapon_field_type_for_fighter(self, fighter):
+    def test_first_weapon_field_type_for_fighter(self):
         assert isinstance(self.form.fields["first_weapon"], forms.ChoiceField)
 
-    def test_amor_field_does_not_exist_for_fighter(self, fighter):
+    def test_amor_field_does_not_exist_for_fighter(self):
         assert "armor" not in self.form.fields
 
-    def test_second_weapon_field_exists_for_fighter(self, fighter):
+    def test_second_weapon_field_exists_for_fighter(self):
         assert "second_weapon" in self.form.fields
 
-    def test_second_weapon_field_type_for_fighter(self, fighter):
+    def test_second_weapon_field_type_for_fighter(self):
         assert isinstance(self.form.fields["second_weapon"], forms.ChoiceField)
 
-    def test_third_weapon_field_exists_for_fighter(self, fighter):
+    def test_third_weapon_field_exists_for_fighter(self):
         assert "third_weapon" in self.form.fields
 
-    def test_third_weapon_field_type_for_fighter(self, fighter):
+    def test_third_weapon_field_type_for_fighter(self):
         assert isinstance(self.form.fields["third_weapon"], forms.ChoiceField)
 
-    def test_pack_field_exists_for_fighter(self, fighter):
+    def test_pack_field_exists_for_fighter(self):
         assert "pack" in self.form.fields
 
-    def test_pack_field_type_for_fighter(self, fighter):
+    def test_pack_field_type_for_fighter(self):
         assert isinstance(self.form.fields["pack"], forms.ChoiceField)
 
-    def test_field_order(self, fighter):
+    def test_field_order(self):
         assert list(self.form.fields.keys()) == [
             "first_weapon",
             "second_weapon",
@@ -102,35 +102,35 @@ class TestFighterEquipmentSelectForm:
 class TestRogueEquipmentSelectForm:
     form = None
 
-    @pytest.fixture
+    @pytest.fixture(autouse=True)
     def rogue(self):
         self.form = RogueEquipmentSelectForm()
 
-    def test_first_weapon_field_exists_for_rogue(self, rogue):
+    def test_first_weapon_field_exists_for_rogue(self):
         assert "first_weapon" in self.form.fields
 
-    def test_first_weapon_field_type_for_rogue(self, rogue):
+    def test_first_weapon_field_type_for_rogue(self):
         assert isinstance(self.form.fields["first_weapon"], forms.ChoiceField)
 
-    def test_amor_field_does_not_exist_for_rogue(self, rogue):
+    def test_amor_field_does_not_exist_for_rogue(self):
         assert "armor" not in self.form.fields
 
-    def test_second_weapon_field_exists_for_rogue(self, rogue):
+    def test_second_weapon_field_exists_for_rogue(self):
         assert "second_weapon" in self.form.fields
 
-    def test_second_weapon_field_type_for_rogue(self, rogue):
+    def test_second_weapon_field_type_for_rogue(self):
         assert isinstance(self.form.fields["second_weapon"], forms.ChoiceField)
 
-    def test_third_weapon_field_does_not_exist_for_rogue(self, rogue):
+    def test_third_weapon_field_does_not_exist_for_rogue(self):
         assert "third_weapon" not in self.form.fields
 
-    def test_pack_field_exists_for_rogue(self, rogue):
+    def test_pack_field_exists_for_rogue(self):
         assert "pack" in self.form.fields
 
-    def test_pack_field_type_for_rogue(self, rogue):
+    def test_pack_field_type_for_rogue(self):
         assert isinstance(self.form.fields["pack"], forms.ChoiceField)
 
-    def test_field_order(self, rogue):
+    def test_field_order(self):
         assert list(self.form.fields.keys()) == [
             "first_weapon",
             "second_weapon",
@@ -142,38 +142,38 @@ class TestRogueEquipmentSelectForm:
 class TestWizardEquipmentSelectForm:
     form = None
 
-    @pytest.fixture
+    @pytest.fixture(autouse=True)
     def wizard(self):
         self.form = WizardEquipmentSelectForm()
 
-    def test_first_weapon_field_exists_for_wizard(self, wizard):
+    def test_first_weapon_field_exists_for_wizard(self):
         assert "first_weapon" in self.form.fields
 
-    def test_first_weapon_field_type_for_wizard(self, wizard):
+    def test_first_weapon_field_type_for_wizard(self):
         assert isinstance(self.form.fields["first_weapon"], forms.ChoiceField)
 
-    def test_amor_field_does_not_exist_for_wizard(self, wizard):
+    def test_amor_field_does_not_exist_for_wizard(self):
         assert "armor" not in self.form.fields
 
-    def test_second_weapon_field_does_not_exist_for_wizard(self, wizard):
+    def test_second_weapon_field_does_not_exist_for_wizard(self):
         assert "second_weapon" not in self.form.fields
 
-    def test_third_weapon_field_does_not_exist_for_wizard(self, wizard):
+    def test_third_weapon_field_does_not_exist_for_wizard(self):
         assert "third_weapon" not in self.form.fields
 
-    def test_pack_field_exists_for_wizard(self, wizard):
+    def test_pack_field_exists_for_wizard(self):
         assert "pack" in self.form.fields
 
-    def test_pack_field_type_for_wizard(self, wizard):
+    def test_pack_field_type_for_wizard(self):
         assert isinstance(self.form.fields["pack"], forms.ChoiceField)
 
-    def test_gear_field_exists_for_wizard(self, wizard):
+    def test_gear_field_exists_for_wizard(self):
         assert "gear" in self.form.fields
 
-    def test_gear_field_type_for_wizard(self, wizard):
+    def test_gear_field_type_for_wizard(self):
         assert isinstance(self.form.fields["gear"], forms.ChoiceField)
 
-    def test_field_order(self, wizard):
+    def test_field_order(self):
         assert list(self.form.fields.keys()) == [
             "first_weapon",
             "gear",
