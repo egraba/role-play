25cf95d5a37f6e6303aad91fe3185bf415a04117
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-01-13 07:27:18 +0100

    test(character): fix tests

--- character/tests/views/conftest.py
+++ character/tests/views/conftest.py
@@ -5,35 +5,35 @@ from character.models.classes import Class
 from ..factories import CharacterFactory
 
 
-@pytest.fixture()
+@pytest.fixture
 def character(client):
     character = CharacterFactory(name="user")
     client.force_login(character.user)
     return character
 
 
-@pytest.fixture()
+@pytest.fixture
 def cleric(client):
     character = CharacterFactory(name="cleric", class_name=Class.CLERIC)
     client.force_login(character.user)
     return character
 
 
-@pytest.fixture()
+@pytest.fixture
 def fighter(client):
     character = CharacterFactory(name="fighter", class_name=Class.FIGHTER)
     client.force_login(character.user)
     return character
 
 
-@pytest.fixture()
+@pytest.fixture
 def rogue(client):
     character = CharacterFactory(name="rogue", class_name=Class.ROGUE)
     client.force_login(character.user)
     return character
 
 
-@pytest.fixture()
+@pytest.fixture
 def wizard(client):
     character = CharacterFactory(name="wizard", class_name=Class.WIZARD)
     client.force_login(character.user)
