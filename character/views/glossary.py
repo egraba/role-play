1d6cb654dbe05516a24c45d83d0d160cc8bdc763
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-01-20 09:00:45 +0100

    feat(character): use cache

--- character/views/glossary.py
+++ character/views/glossary.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.views.generic import TemplateView
 
 from character.models.abilities import AbilityType
@@ -9,6 +10,17 @@ class GlossaryView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["ability_types"] = AbilityType.objects.all()
-        context["skills"] = Skill.objects.all()
+
+        ability_types = cache.get("ability_types")
+        if not ability_types:
+            ability_types = AbilityType.objects.all()
+            cache.set("ability_types", ability_types)
+        context["ability_types"] = ability_types
+
+        skills = cache.get("skills")
+        if not skills:
+            skills = Skill.objects.all()
+            cache.set("skills", skills)
+        context["skills"] = skills
+
         return context
