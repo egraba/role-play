ac323460551aee24596ec2106cd5bc66390ee099
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-01-20 07:13:58 +0100

    feat(character): fill glossary page with content

--- character/views/glossary.py
+++ character/views/glossary.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
 
+from character.models.abilities import AbilityType
+from character.models.skills import Skill
+
 
 class GlossaryView(TemplateView):
     template_name = "character/glossary.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["ability_types"] = AbilityType.objects.all()
+        context["skills"] = Skill.objects.all()
+        return context
