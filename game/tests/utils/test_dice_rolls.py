f01e8f0641af036d0c5eaafe86e0013cb0ac4719
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-02-02 20:24:01 +0100

    test(game): remove class usage

--- game/tests/utils/test_dice_rolls.py
+++ game/tests/utils/test_dice_rolls.py
@@ -7,15 +7,13 @@ from game.utils.dice_rolls import perform_ability_check
 
 
 @pytest.mark.django_db
-class TestPerformAbilityCheck:
-    def test_perform_ability_check(self):
-        character = CharacterFactory()
-        ability_check_request = AbilityCheckRequestFactory(character=character)
-
-        # Perform the test several time, as it uses random values.
-        for _ in range(10):
-            score, result = perform_ability_check(character, ability_check_request)
-            if score >= ability_check_request.difficulty_class:
-                assert result == AbilityCheck.Result.SUCCESS
-            else:
-                assert result == AbilityCheck.Result.FAILURE
+def test_perform_ability_check():
+    character = CharacterFactory()
+    ability_check_request = AbilityCheckRequestFactory(character=character)
+    # Perform the test several time, as it uses random values.
+    for _ in range(10):
+        score, result = perform_ability_check(character, ability_check_request)
+        if score >= ability_check_request.difficulty_class:
+            assert result == AbilityCheck.Result.SUCCESS
+        else:
+            assert result == AbilityCheck.Result.FAILURE
