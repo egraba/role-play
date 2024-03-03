9b10d53b9847c6fe9e2803f58b6bf933bf75a1ac
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-03-03 10:08:27 +0100

    feat(character): add androgynous gender

--- character/constants/character.py
+++ character/constants/character.py
@@ -4,4 +4,4 @@ from django.db.models import TextChoices
 class Gender(TextChoices):
     MALE = "M", "Male"
     FEMALE = "F", "Female"
-    UNDEFINED = "U", "Undefined"
+    ANDROGYNOUS = "A", "Androgynous"
