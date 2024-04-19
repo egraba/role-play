4a1a76afc97d68ca190b1678816c656fb5857e54
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-04-19 16:57:20 +0200

    implement bonus

--- character/tests/utils/equipment/test_parsers.py
+++ character/tests/utils/equipment/test_parsers.py
@@ -1,6 +1,28 @@
+import pytest
+from faker import Faker
+
 from character.utils.equipment.parsers import parse_ac_settings
 
 
 def test_parse_ac_settings_ac_only():
     settings_str = "11"
     assert parse_ac_settings(settings_str) == (11, 0, 0)
+
+
+def test_parse_ac_settings_ac_only_wrong_format():
+    fake = Faker()
+    settings_str = fake.random_letter()
+    with pytest.raises(ValueError):
+        parse_ac_settings(settings_str)
+
+
+def test_parse_ac_settings_plus_prefix():
+    settings_str = "+2"
+    assert parse_ac_settings(settings_str) == (0, 0, 2)
+
+
+def test_parse_ac_settings_plus_prefix_wrong_format():
+    fake = Faker()
+    settings_str = f"+{fake.random_letters()}"
+    with pytest.raises(ValueError):
+        parse_ac_settings(settings_str)
--- character/utils/equipment/parsers.py
+++ character/utils/equipment/parsers.py
@@ -1,6 +1,11 @@
 def parse_ac_settings(settings: str) -> tuple[int, int, int]:
-    array = settings.split()
-    base_ac = int(array[0])
+    base_ac = 0
     dexterity_modifier = 0
     bonus = 0
+    array = settings.split()
+    first_part = array[0]
+    if first_part.startswith("+"):
+        bonus = int(first_part)
+    else:
+        base_ac = int(array[0])
     return base_ac, dexterity_modifier, bonus
