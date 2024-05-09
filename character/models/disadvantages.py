7403001e48c1931c89337be95248e8c30929b76f
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-05-09 14:46:51 +0200

    create disadvantage models

--- character/models/disadvantages.py
+++ character/models/disadvantages.py
@@ -10,3 +10,16 @@ class DexterityCheckDisadvantage(models.Model):
 class AbilityCheckDisadvantage(models.Model):
     character = models.ForeignKey("character.Character", on_delete=models.CASCADE)
     ability_type = models.ForeignKey(AbilityType, on_delete=models.CASCADE)
+
+
+class SavingThrowDisadvantage(models.Model):
+    character = models.ForeignKey("character.Character", on_delete=models.CASCADE)
+
+
+class AttackRollDisadvantage(models.Model):
+    character = models.ForeignKey("character.Character", on_delete=models.CASCADE)
+    ability_type = models.ForeignKey(AbilityType, on_delete=models.CASCADE)
+
+
+class SpellCastDisadvantage(models.Model):
+    character = models.ForeignKey("character.Character", on_delete=models.CASCADE)
