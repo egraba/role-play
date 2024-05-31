41a2e6cc30cf8d84196445139641043d561a538b
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-05-31 15:19:58 +0200

    remove useless lines

--- character/management/commands/delete_character.py
+++ character/management/commands/delete_character.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand, CommandError
 
 from ...models.character import Character
@@ -15,11 +14,7 @@ class Command(BaseCommand):
         username = options["user"]
         try:
             user = User.objects.get(username=username)
-        except ObjectDoesNotExist as e:
-            raise CommandError(f"[{username}] doesn't exist...") from e
-        try:
-            Character.objects.filter(user=user).delete()
-        except ObjectDoesNotExist as e:
-            raise CommandError(f"[{username}] doesn't have a character...") from e
-
+        except User.DoesNotExist as exc:
+            raise CommandError(f"[{username}] doesn't exist") from exc
+        Character.objects.filter(user=user).delete()
         self.stdout.write(self.style.SUCCESS("Successfully deleted the character"))
