c466f3da76f5cbfe24858407b058534db4b9d21b
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-06-01 10:13:46 +0200

    display inventory in character view

--- character/views/character.py
+++ character/views/character.py
@@ -15,7 +15,7 @@ class CharacterDetailView(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["inventory"] = None
+        context["inventory"] = self.object.inventory
         context["abilities"] = self.object.abilities.all()
         return context
 
