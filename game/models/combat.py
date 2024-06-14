76fa6318ad1f8fa7b352ca9999dbdf279e02731f
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-06-14 19:52:52 +0200

    add foreign keys in models

--- game/models/combat.py
+++ game/models/combat.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+from .game import Game, Player
+
 
 class Combat(models.Model):
-    pass
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
 
 
 class Round(models.Model):
-    pass
+    combat = models.ForeignKey(Combat, on_delete=models.CASCADE)
 
 
 class Turn(models.Model):
-    pass
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    round = models.ForeignKey(Round, on_delete=models.CASCADE)
