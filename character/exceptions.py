0299ffa9a11e42de8eadcdb5d0404e4a9370aed4
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-06-29 16:07:33 +0200

    remove unused exception

--- character/exceptions.py
+++ character/exceptions.py
@@ -1,13 +1,2 @@
-from django.core.exceptions import ObjectDoesNotExist
-
-
-class AbilityNotFound(ObjectDoesNotExist):
-    """Raised when an ability is not found."""
-
-    def __init__(self, ability_type):
-        self.message = f"[{ability_type}] is not found..."
-        super().__init__(self.message)
-
-
 class EquipmentDoesNotExist(Exception):
     pass
