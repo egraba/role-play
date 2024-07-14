68219529df1e8132b981601e1628befdca281c32
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-07-14 19:33:58 +0200

    create test for CombatCreateForm

--- game/tests/test_forms.py
+++ game/tests/test_forms.py
@@ -1,6 +1,7 @@
+import pytest
 from django import forms
 
-from game.forms import QuestCreateForm
+from game.forms import CombatCreateForm, QuestCreateForm
 
 
 class TestQuestCreateForm:
@@ -11,3 +12,11 @@ class TestQuestCreateForm:
     def test_content_max_length(self):
         form = QuestCreateForm()
         assert form.fields["content"].max_length == 1000
+
+
+pytestmark = pytest.mark.django_db
+
+
+class TestCombatCreateForm:
+    def test_character_fields(self, started_game):
+        CombatCreateForm(initial={"game": f"{started_game.id}"})
