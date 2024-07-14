8ef9b9f5c23ecfb1a0590167954c80f62003948a
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-07-14 19:18:31 +0200

    remove useless test

--- game/tests/test_forms.py
+++ game/tests/test_forms.py
@@ -1,6 +1,5 @@
 from django import forms
 
-from character.forms.character import CharacterCreateForm
 from game.forms import QuestCreateForm
 
 
@@ -12,17 +11,3 @@ class TestQuestCreateForm:
     def test_content_max_length(self):
         form = QuestCreateForm()
         assert form.fields["content"].max_length == 1000
-
-
-class TestCharacterCreateForm:
-    def test_name_type(self):
-        form = CharacterCreateForm()
-        assert isinstance(form.fields["name"].widget, forms.TextInput)
-
-    def test_name_max_length(self):
-        form = CharacterCreateForm()
-        assert form.fields["name"].max_length == 100
-
-    def test_race_type(self):
-        form = CharacterCreateForm()
-        assert isinstance(form.fields["race"].widget, forms.Select)
