497baf0df50cdfe3cf88bab261c30db6e3563363
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-07-15 19:15:40 +0200

    create format_message() function

--- game/channel_messages.py
+++ game/channel_messages.py
@@ -1,2 +1,4 @@
-def display(message: str) -> str:
-    return message
+def format_message(message: str, sender_name: str = "") -> str:
+    if sender_name is None:
+        sender_name = "the Master"
+    return f"{sender_name} said: {message}"
--- game/tests/test_channel_messages.py
+++ game/tests/test_channel_messages.py
@@ -1,6 +1,20 @@
-from game.channel_messages import display
+import pytest
+from faker import Faker
+
+from game.channel_messages import format_message
 
 
-def test_display():
-    some_message = "some message"
-    assert display(some_message) == some_message
+@pytest.fixture
+def fake():
+    return Faker()
+
+
+def test_format_message_from_master(fake):
+    message = fake.text()
+    assert format_message(message) == f"the Master said: {message}"
+
+
+def test_format_message_from_player(fake):
+    message = fake.text()
+    sender = fake.first_name()
+    assert format_message(message, sender) == f"{sender} said: {message}"
