97209674bbbbfe3f86ebbc67c8952e78e6d196f2
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-07-16 15:12:29 +0200

    simplify send_to-channel()

--- game/utils/channels.py
+++ game/utils/channels.py
@@ -1,27 +1,22 @@
-from typing import Any
-
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
-from pydantic import ValidationError
 
-from ..schemas import GameEvent, GameEventError, GameEventOrigin
+from ..models.events import Event
+from ..schemas import GameEvent, GameEventOrigin, PlayerType
 
 
-def send_to_channel(game_id: int, game_event: dict[str, Any]) -> None:
+def send_to_channel(event: Event) -> None:
     """
-    Send game events to the game channel layer.
-
-    Args:
-        game_id (int): Game identifier.
-        game_event (dict[str, Any]): Game event.
+    Serialize an game event to a JSON and send it in the right channel.
     """
-    # All the events sent by this function are server-side events.
-    game_event["origin"] = GameEventOrigin.SERVER_SIDE
-    try:
-        GameEvent(**game_event)
-    except ValidationError as exc:
-        raise GameEventError(exc.errors()) from exc
+    game_event = GameEvent(
+        type=None,
+        date=event.date,
+        player_type=PlayerType.MASTER,
+        message=event.message,
+        origin=GameEventOrigin.SERVER_SIDE,
+    )
     channel_layer = get_channel_layer()
     async_to_sync(channel_layer.group_send)(
-        group=f"game_{game_id}_events", message=game_event
+        group=f"game_{event.game.id}_events", message=game_event
     )
