f6f39b2090a0b08875a87a1329f942d9427437a0
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-07-16 16:46:42 +0200

    send_to_channel() automatically retrieves quest updates

--- game/utils/channels.py
+++ game/utils/channels.py
@@ -1,8 +1,17 @@
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
-from ..models.events import Event
-from ..schemas import EventSchema, EventOrigin, PlayerType
+from ..models.events import Event, Quest
+from ..schemas import EventSchema, EventOrigin, PlayerType, EventType
+
+
+def _get_event_type(event: Event) -> EventType:
+    """
+    Retrieve event type according to Event instance class.
+    """
+    if isinstance(event, Quest):
+        event_type = EventType.QUEST_UPDATE
+    return event_type
 
 
 def send_to_channel(event: Event) -> None:
@@ -10,7 +19,7 @@ def send_to_channel(event: Event) -> None:
     Serialize an game event to a JSON and send it in the right channel.
     """
     game_event = EventSchema(
-        type=None,
+        type=_get_event_type(event),
         date=event.date,
         player_type=PlayerType.MASTER,
         message=event.message,
