86f4c868a7a5183d46a445e69c69cd40b44168b4
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-07-29 21:34:27 +0200

    implement get_message() for GameStart event

--- game/messages.py
+++
@@ -1,8 +0,0 @@
-from .models.events import Event, QuestUpdate
-
-
-def get_message(event: Event) -> str:
-    message = ""
-    if isinstance(event, QuestUpdate):
-        message = "The Master created the campaign."
-    return message
