f73560d9e510b65e5c6eb5f7396f1dff959dafd7
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-08-02 12:31:24 +0200

    add more comment

--- game/event_enrichers.py
+++ game/event_enrichers.py
@@ -6,12 +6,24 @@ from .schemas import EventSchema
 
 
 class Enricher:
+    """
+    Enricher class.
+
+    To have a better speed, event messages need to be enriched before the events are
+    actually stored in the database.
+
+    Attributes:
+        game (Game): Current game instance.
+        content (EventSchema): content to enrich.
+    """
+
     def __init__(self, game: Game, content: EventSchema, *args, **kwargs):
         self.game = game
         self.content = content
 
     @abstractmethod
     def enrich(self):
+        """Enrich event messages."""
         pass
 
 
