1450044134737cf799e60183b866c52bea80beae
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-08-24 16:58:27 +0200

    make fixtures loading more dynamic

--- src/role_play/character/tests/conftest.py
+++ src/role_play/character/tests/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from django.core.management import call_command
 from django.core.management.commands import loaddata
@@ -6,10 +8,14 @@ from django.core.management.commands import loaddata
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
-        call_command(loaddata.Command(), "character/fixtures/senses.yaml")
-        call_command(loaddata.Command(), "character/fixtures/advancement.yaml")
-        call_command(loaddata.Command(), "character/fixtures/abilities.yaml")
-        call_command(loaddata.Command(), "character/fixtures/languages.yaml")
-        call_command(loaddata.Command(), "character/fixtures/klasses.yaml")
-        call_command(loaddata.Command(), "character/fixtures/skills.yaml")
-        call_command(loaddata.Command(), "character/fixtures/equipment.yaml")
+        fixtures_path = os.environ["PYTHONPATH"] + "/character/fixtures/"
+        for fixture in [
+            "senses",
+            "advancement",
+            "abilities",
+            "languages",
+            "klasses",
+            "skills",
+            "equipment",
+        ]:
+            call_command(loaddata.Command(), f"{fixtures_path}{fixture}.yaml")
