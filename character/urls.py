0525bd24ae692842af8647ecc8e4557b3ee3ac79
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-09-14 10:18:22 +0200

    make transition to skills selection

--- character/urls.py
+++ character/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-from .forms.character import CharacterCreateForm
 from .views.backgrounds import BackgroundCompleteView
 from .views.character import CharacterCreateView, CharacterDetailView, CharacterListView
 from .views.equipment import EquipmentSelectView
@@ -20,7 +19,7 @@ urlpatterns = [
     ),
     path(
         "create_character",
-        CharacterCreateView.as_view([CharacterCreateForm]),
+        CharacterCreateView.as_view(),
         name="character-create",
     ),
     path(
--- character/views/character.py
+++ character/views/character.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from django.views.generic import DetailView, ListView
 from formtools.wizard.views import SessionWizardView
 
+from ..forms.character import CharacterCreateForm
+from ..forms.skills import SkillsSelectForm
 from ..models.character import Character
 
 
@@ -26,7 +28,16 @@ class CharacterListView(LoginRequiredMixin, ListView):
 
 
 class CharacterCreateView(LoginRequiredMixin, SessionWizardView):
+    form_list = [CharacterCreateForm, SkillsSelectForm]
     template_name = "character/character_create.html"
+
+    def get_form_initial(self, step):
+        if step == "1":
+            data = self.storage.get_step_data("0")
+            if data:
+                klass = data.get("0-klass")
+                return self.initial_dict.get(step, {"klass": klass})
+        return self.initial_dict.get(step, {})
 
     def done(self, form_list, **kwargs):
         return HttpResponseRedirect(reverse("index"))
