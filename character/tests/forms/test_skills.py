f3c42350da6e1239ebb8d22dc76b826f619f3111
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2024-09-15 12:10:16 +0200

    fix skills forms tests

--- character/tests/forms/test_skills.py
+++ character/tests/forms/test_skills.py
@@ -1,27 +1,81 @@
-from character.forms.skills import ExtendedSkillsSelectForm, SkillsSelectForm
+import pytest
+from django import forms
+
+from character.constants.klasses import Klass
+from character.forms.skills import SkillsSelectForm
 
 
-class TestSkillsSelectForm:
-    form = SkillsSelectForm(initial={"klass": None})
-
-    def test_first_skill_field_exists(self):
-        assert "first_skill" in self.form.fields
-
-    def test_second_skill_field_exists(self):
-        assert "second_skill" in self.form.fields
+@pytest.fixture
+def cleric_form():
+    return SkillsSelectForm(initial={"klass": Klass.CLERIC})
 
 
-class TestExtendedSkillsSelectForm:
-    form = ExtendedSkillsSelectForm(initial={"klass": None})
+@pytest.fixture
+def cleric_fields():
+    return ["first_skill", "second_skill"]
 
-    def test_first_skill_field_exists(self):
-        assert "first_skill" in self.form.fields
 
-    def test_second_skill_field_exists(self):
-        assert "second_skill" in self.form.fields
+def test_cleric_fields_presence(cleric_form, cleric_fields):
+    assert cleric_fields == list(cleric_form.fields)
 
-    def test_third_skill_field_exists(self):
-        assert "third_skill" in self.form.fields
 
-    def test_fourth_skill_field_exists(self):
-        assert "fourth_skill" in self.form.fields
+def test_cleric_fields_type(cleric_form):
+    for field in cleric_form.fields:
+        assert isinstance(cleric_form.fields[field], forms.ChoiceField)
+
+
+@pytest.fixture
+def fighter_form():
+    return SkillsSelectForm(initial={"klass": Klass.FIGHTER})
+
+
+@pytest.fixture
+def fighter_fields():
+    return ["first_skill", "second_skill"]
+
+
+def test_fighter_fields_presence(fighter_form, fighter_fields):
+    assert fighter_fields == list(fighter_form.fields)
+
+
+def test_fighter_fields_type(fighter_form):
+    for field in fighter_form.fields:
+        assert isinstance(fighter_form.fields[field], forms.ChoiceField)
+
+
+@pytest.fixture
+def rogue_form():
+    return SkillsSelectForm(initial={"klass": Klass.ROGUE})
+
+
+@pytest.fixture
+def rogue_fields():
+    return ["first_skill", "second_skill", "third_skill", "fourth_skill"]
+
+
+def test_rogue_fields_presence(rogue_form, rogue_fields):
+    assert rogue_fields == list(rogue_form.fields)
+
+
+def test_rogue_fields_type(rogue_form):
+    for field in rogue_form.fields:
+        assert isinstance(rogue_form.fields[field], forms.ChoiceField)
+
+
+@pytest.fixture
+def wizard_form():
+    return SkillsSelectForm(initial={"klass": Klass.WIZARD})
+
+
+@pytest.fixture
+def wizard_fields():
+    return ["first_skill", "second_skill"]
+
+
+def test_wizard_fields_presence(wizard_form, wizard_fields):
+    assert wizard_fields == list(wizard_form.fields)
+
+
+def test_wizard_fields_type(wizard_form):
+    for field in wizard_form.fields:
+        assert isinstance(wizard_form.fields[field], forms.ChoiceField)
