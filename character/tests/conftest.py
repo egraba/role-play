69f1a3526940b98e1eaf903cb77dce161cdb5a14
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2025-01-10 18:01:40 +0100

    add tests for CharacterContextMixin

--- character/tests/conftest.py
+++ character/tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from django.core.management import call_command
 from django.core.management.commands import loaddata
+from .factories import CharacterFactory
 
 
 @pytest.fixture(scope="session")
@@ -13,3 +14,8 @@ def django_db_setup(django_db_setup, django_db_blocker):
         call_command(loaddata.Command(), "character/fixtures/klasses.yaml")
         call_command(loaddata.Command(), "character/fixtures/skills.yaml")
         call_command(loaddata.Command(), "character/fixtures/equipment.yaml")
+
+
+@pytest.fixture(scope="function")
+def character():
+    return CharacterFactory()
