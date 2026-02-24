a66f2998419f8f027e103fc23ecc2c0bbbb96799
Eric Grabarczyk <eric.grabarczyk@gmail.com>
2026-02-24 20:11:27 +0100

    fix: move UserFactory import to top, add skipped get_absolute_url test

--- adventure/tests/test_models.py
+++ adventure/tests/test_models.py
@@ -1,6 +1,7 @@
 import pytest
 
 from adventure.tests.factories import CampaignFactory
+from user.tests.factories import UserFactory
 
 
 @pytest.mark.django_db
@@ -17,8 +18,13 @@ def test_campaign_slug_auto_generated():
 
 @pytest.mark.django_db
 def test_campaign_has_owner():
-    from user.tests.factories import UserFactory
-
     user = UserFactory()
     campaign = CampaignFactory(owner=user)
     assert campaign.owner == user
+
+
+@pytest.mark.django_db
+@pytest.mark.skip(reason="URL config registered in Task 9")
+def test_campaign_get_absolute_url():
+    campaign = CampaignFactory(title="Test Campaign")
+    assert campaign.get_absolute_url() == f"/adventure/{campaign.slug}/"
